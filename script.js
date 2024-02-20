797410536a570bfabb344554c6d565fe34b25cd4
Miriam Fuchs <fuchsmiriam@yahoo.com>
2024-02-20 17:13:09 +0100

    Basket revised

--- script.js
+++ script.js
@@ -2,56 +2,60 @@ let dishes = [
     {
         "name": "Frühlingsrollen",
         "description": "Beliebter Klassiker der chinesischen Küche. Wahlweise mit süß-saurer Sauce.",
-        "price": "5,50 €"
+        "price": 5.50
     },
     {
         "name": "Tofu mit Broccoli in Knoblauchsoße",
         "description": "Gebratener Tofu und frischer Broccoli in einer würzigen Knoblauchsoße, serviert mit Reis.",
-        "price": "9,00 €"
+        "price": 9.00
     },
     {
         "name": "Wan-Tan-Suppe",
         "description": "Feine Teigtaschen gefüllt mit würziger Füllung, serviert in einer klaren Brühe mit frischen Gemüsestreifen.",
-        "price": "6,50 €"
+        "price": 6.50
     },
     {
         "name": "Gebratener Reis mit Gemüse",
         "description": "Zarter, aromatischer Reis kombiniert mit einer bunten Auswahl an frischem Gemüse.",
-        "price": "11,20 €"
+        "price": 11.20
     },
     {
         "name": "Pekingente",
         "description": "Knusprig gebratene Ente mit dünner Haut und zartem Fleisch, serviert mit Pfannkuchen, Gemüse und Hoisin-Sauce.",
-        "price": "12,90 €"
+        "price": 12.90
     },
     {
         "name": "Kung Pao Hühnchen",
         "description": "Geschmackvolles Hühnchen, mariniert in einer würzigen Sauce mit knackigen Erdnüssen, Paprika und Frühlingszwiebeln.",
-        "price": "9,80 €"
+        "price": 9.80
     },
     {
         "name": "Gedämpfter Fisch mit Ingwer und Frühlingszwiebeln",
         "description": "Zarter Fisch, schonend gedämpft und mit frischem Ingwer sowie Frühlingszwiebeln aromatisiert.",
-        "price": "10,50 €"
+        "price": 10.50
     },
     {
         "name": "Gebackene Banane",
         "description": "Reife Bananen, umhüllt von knusprigem Teig und goldbraun frittiert.",
-        "price": "7,00 €"
+        "price": 7.00
     },
     {
         "name": "Mango Sticky Rice",
         "description": "Süßer klebriger Reis, serviert mit frischen Mangoscheiben und einer Kokosmilchsoße.",
-        "price": "6,50 €"
+        "price": 6.50
     }
 ];
 
 
 let basketItems = [];
 
+let amount = [];
+
 
 document.addEventListener("DOMContentLoaded", function () {
+    loadBasket();
     showDishes();
+    showBasket();
 });
 
 
@@ -71,7 +75,7 @@ function showDishes() {
                         <br>
                         <span class="dishDescription">${dish.description}</span>
                         <br>
-                        <span class="dishPrice">${dish.price}</span>
+                        <span class="dishPrice">${dish.price.toFixed(2).replace('.', ',')} €</span>
                     </div>
                 </div>
             `;
@@ -84,6 +88,7 @@ function showDishes() {
 
 function addToBasket(index) {
     basketItems.push(dishes[index]);
+    saveBasket();
     showBasket();
 }
 
@@ -97,9 +102,10 @@ function showBasket() {
             basket.innerHTML += `<div class="card card-body cardDistance">
                 <div class="basketContent">
                 <p>${item.name}</p>
-                <p>${item.price}</p></div>
+                <p>${item.price.toFixed(2).replace('.', ',')} €</p></div>
                 <div class="deleteContainer">
-                    <img class="garbageImage" src="./img/garbage.png" alt="Mülleimer" onclick="deleteDish(${i})">
+                    <img class="garbageImage" src="./img/minusknopf.png" alt="Minus" onclick="deleteAmount(${i})">
+                    <img class="garbageImage" src="./img/plus.png" alt="Plus" onclick="addAmount(${i})">
                 </div>
             </div>`;
         });
@@ -115,7 +121,38 @@ function showBasket() {
 }
 
 
-function deleteDish(i) {
+function saveBasket() {
+    localStorage.setItem('basketItems', JSON.stringify(basketItems));
+}
+
+
+function loadBasket() {
+    let savedBasket = localStorage.getItem('basketItems');
+    if (savedBasket) {
+        basketItems = JSON.parse(savedBasket);
+    }
+}
+
+
+function deleteAmount(i) {
     basketItems.splice(i, 1);
+    saveBasket();
     showBasket();
+}
+
+
+function addAmount(){
+
+}
+
+
+function updateShoppingBasket() {
+    let sum = 0;
+    for (let i = 0; i < basketItems.length; i++) {
+        sum += basketItems[i].price * basketItems[i].amount;
+    }
+
+    let finalSum = sum + 2.50;
+
+    document.getElementById('totalSum').innerHTML = finalSum.toFixed(2).replace('.', ',') + " €";
 }
